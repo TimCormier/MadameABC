81788a317345c7adece4dfaf870e12aecd0f1e3a
Thomas <t.rompre@student.isartdigital.com>
2020-05-04 16:29:04 -0400

    Working navigation

--- Assets/Script/Thomas/ClickableScript.cs
+++ Assets/Script/Thomas/ClickableScript.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClickableScript : MonoBehaviour
 {
+    private Button btn;
+    private string name;
+    private GameObject MANAGER;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        btn = gameObject.transform.GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+        name = gameObject.transform.name;
+        MANAGER = GameObject.Find("background and GameManager");
     }
 
     // Update is called once per frame
@@ -20,8 +28,43 @@ public class ClickableScript : MonoBehaviour
     }
 
     void OnMouseDown() {
-        Debug.Log("You Clicked" + gameObject.transform.name);
+        Debug.Log("You Clicked" + gameObject.transform.name);//
     }
 
+    void OnClick() {
+        Debug.Log("You have clicked " + gameObject.transform.name);
+
+        switch (name) {
+            case "DoorMusic":
+                Debug.Log("Going to music room");
+                MANAGER.GetComponent<NavScript>().MUSIC();
+                break;
+
+            case "BackButton":
+                Debug.Log("clicked on backbutton");
+                MANAGER.GetComponent<NavScript>().MAIN();
+                break;
+
+            case "DoorClass":
+                MANAGER.GetComponent<NavScript>().CLASS();
+                break;
+
+            case "DoorLibrary":
+                MANAGER.GetComponent<NavScript>().LIBRARY();
+                break;
+
+            case "DoorYoga":
+                MANAGER.GetComponent<NavScript>().YOGA();
+                break;
+
+            case "DoorCraft":
+                MANAGER.GetComponent<NavScript>().CRAFT();
+                break;
+
+            default:
+                Debug.Log("idk what that is chief");
+                break;
+        }
+    }
     
 }
--- Assets/Script/Thomas/NavScript.cs
+++ Assets/Script/Thomas/NavScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NavScript : MonoBehaviour
 {
@@ -11,15 +12,28 @@ public class NavScript : MonoBehaviour
     private GameObject DOORLIBRARY;
     private GameObject DOORYOGA;
     private GameObject DOORCRAFT;
+    private GameObject BACKBUTTON;
+
+    private RawImage rawimg;
+    public Texture defaultTexture;
+    public Texture MusicRoom;
+    public Texture ClassRoom;
+    public Texture LibraryRoom;
+    public Texture YogaRoom;
+    public Texture CraftRoom;
     
     // Start is called before the first frame update
     void Start()
     {
+        rawimg = gameObject.GetComponent<RawImage>();
+        rawimg.texture = defaultTexture;
         DOORMUSIC = GameObject.Find("DoorMusic");
         DOORCLASS = GameObject.Find("DoorClass");
         DOORLIBRARY = GameObject.Find("DoorLibrary");
         DOORYOGA = GameObject.Find("DoorYoga");
         DOORCRAFT = GameObject.Find("DoorCraft");
+        BACKBUTTON = GameObject.Find("BackButton");
+        BACKBUTTON.SetActive(false);
 
 
     }
@@ -27,7 +41,8 @@ public class NavScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //following code is not mine
+       
+        /*//following code is not mine
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("Mouseclick");
@@ -41,7 +56,65 @@ public class NavScript : MonoBehaviour
                 Debug.Log(hit.collider.gameObject.name);
                 hit.collider.attachedRigidbody.AddForce(Vector2.up);
             }
-        }
+        }*/
+
+
+    }
+
+    private void HIDE() {
+        DOORMUSIC.SetActive(false);
+        DOORCLASS.SetActive(false);
+        DOORLIBRARY.SetActive(false);
+        DOORYOGA.SetActive(false);
+        DOORCRAFT.SetActive(false);
+        BACKBUTTON.SetActive(true);
+    }
+
+    private void SHOW()
+    {
+        DOORMUSIC.SetActive(true);
+        DOORCLASS.SetActive(true);
+        DOORLIBRARY.SetActive(true);
+        DOORYOGA.SetActive(true);
+        DOORCRAFT.SetActive(true);
+        BACKBUTTON.SetActive(false);
+    }
+
+
+    public void MUSIC() {
+        rawimg.texture = MusicRoom;
+        HIDE();
+   
+    }
+    public void CLASS()
+    {
+        rawimg.texture = ClassRoom;
+        HIDE();
+
+    }
+    public void LIBRARY()
+    {
+        rawimg.texture = LibraryRoom;
+        HIDE();
+
+    }
+    public void YOGA()
+    {
+        rawimg.texture = YogaRoom;
+        HIDE();
+
+    }
+    public void CRAFT()
+    {
+        rawimg.texture = CraftRoom;
+        HIDE();
+
+    }
+
+    public void MAIN()
+    {
+        rawimg.texture = defaultTexture;
+        SHOW();
 
     }
 
