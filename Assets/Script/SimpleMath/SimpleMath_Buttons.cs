5d60b22953cd02e555eca6b498984c72443505e7
Tim <t.cormier@student.isartdigital.com>
2020-05-04 16:49:36 -0400

    SimpleMath Alpha

---
+++ Assets/Script/SimpleMath/SimpleMath_Buttons.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMath_Buttons : MonoBehaviour
+{
+    public SpriteRenderer reponse;
+    public Sprite image1, image2, image3;
+    private int seed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        reponse = GetComponent<SpriteRenderer>();
+        seed = Random.Range(1, 5);
+        Debug.Log(seed);
+    }
+
+    public void SetImage1()
+    {
+        reponse.sprite = image1;
+    }
+    public void SetImage2()
+    {
+        reponse.sprite = image2;
+    }
+    public void SetImage3()
+    {
+        reponse.sprite = image3;
+    }
+}
