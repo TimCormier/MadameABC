f3900926db35c9e34c32dd5f02187ec7f1a011ef
Thomas <t.rompre@student.isartdigital.com>
2020-05-19 15:41:09 -0400

    [Partition] do makes sound, looping works

--- Assets/MiniGames/Partition/script/CursorScript.cs
+++ Assets/MiniGames/Partition/script/CursorScript.cs
@@ -8,6 +8,7 @@ public class CursorScript : MonoBehaviour
 
 
     private GameObject CURSOR;
+    private GameObject LINE;
     private Button PLAYBTN;
     private Vector3 ogpos;
     public float speed;
@@ -17,8 +18,9 @@ public class CursorScript : MonoBehaviour
     void Start()
     {
         CURSOR = GameObject.Find("Cursor");
+        LINE = GameObject.Find("Line");
         PLAYBTN = GameObject.Find("PlayButton").transform.GetComponent<Button>();
-        PLAYBTN.onClick.AddListener(OnClick);
+        PLAYBTN.onClick.AddListener(OnClickRAPENIGGER);
         ogpos = CURSOR.transform.position;
     }
 
@@ -29,19 +31,29 @@ public class CursorScript : MonoBehaviour
             CURSOR.transform.position += new Vector3(speed, 0f, 0f);
 
             //raycast code
+            /*
             RaycastHit hit;
             if (Physics.Raycast(CURSOR.transform.position, transform.TransformDirection(Vector3.down),out hit, 1f)) {
                 Debug.Log(hit.transform.name);
                 //Debug.DrawRay(CURSOR.transform.position, transform.TransformDirection(Vector3.down));
             }
+            */
+
 
             if (CURSOR.transform.position.x >= 7.6) {
-                CURSOR.transform.position = new Vector3(ogpos.x, 0.7f, ogpos.z);
+                if (CURSOR.transform.position.y < ogpos.y)
+                {
+                    CURSOR.transform.position = ogpos;
+                }
+                else {
+                    CURSOR.transform.position = new Vector3(ogpos.x, 0.7f, ogpos.z);
+                }
+              //  CURSOR.transform.position = new Vector3(ogpos.x, 0.7f, ogpos.z);
             }
         }
     }
 
-    void OnClick() {
+    void OnClickRAPENIGGER() {
         Debug.Log("You clicked the play button!");
         playing = true;
     }
---
+++ Assets/MiniGames/Partition/script/NoteScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteScript : MonoBehaviour
+{
+
+    private AudioSource AUDIO;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AUDIO = gameObject.transform.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.transform.name == "Line") {
+            Debug.Log(gameObject.name);
+            AUDIO.Play();
+        }
+    }
+}
+/*
+ switch()
+
+
+
+  */
