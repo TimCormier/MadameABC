f07aec750dd75979ce9dd33af87ee458cb48bca6
Tim <t.cormier@student.isartdigital.com>
2020-05-19 13:38:37 -0400

    [ColorFill]Beta up

---
+++ Assets/MiniGames/ColorFill/Scripts/ColorFill_GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorFill_GameManager : MonoBehaviour
+{
+    public Color selectedColor;
+
+    private void Start()
+    {
+        selectedColor = Color.white;
+    }
+
+    public void setRed()
+    {
+        selectedColor = Color.red;
+    }
+    public void setBlue()
+    {
+        selectedColor = Color.blue;
+    }
+    public void setYellow()
+    {
+        selectedColor = Color.yellow;
+    }
+    public void setGreen()
+    {
+        selectedColor = Color.green;
+    }
+    public void setOrange()
+    {
+        selectedColor = new Color(255,0,100,255);
+    }
+
+}
