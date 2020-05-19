3573681d57ef1689972d258c72135e16dabcf30f
Jérémie Poulin <j.poulin@student.isartdigital.com>
2020-05-19 16:06:34 -0400

    [StoryQuiz] Story Code finished

--- Assets/MiniGames/StoryQuiz/Script/StoryQuiz.cs
+++ Assets/MiniGames/StoryQuiz/Script/StoryQuiz.cs
@@ -1,19 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoryQuiz : MonoBehaviour
 {
-    //public GameObject 
-    // Start is called before the first frame update
+    public GameObject quizBad1, quizBad2;
+    public GameObject quizGood;
+    public GameObject activePart;
+    public GameObject nextButton;
+    public GameObject nextPart;
+
+    private Button goodBtn;
+    private Button badBtn1;
+    private Button badBtn2;
+    private Button nextBtn;
+    
     void Start()
     {
-        
+        minigameStart();
+
+        goodBtn = quizGood.transform.GetComponent<Button>();
+        goodBtn.onClick.AddListener(OnClickGood);
+
+        badBtn1 = quizBad1.transform.GetComponent<Button>();
+        badBtn1.onClick.AddListener(OnClickBad);
+
+        badBtn2 = quizBad2.transform.GetComponent<Button>();
+        badBtn2.onClick.AddListener(OnClickBad);
+
+        nextBtn = nextButton.transform.GetComponent<Button>();
+        nextBtn.onClick.AddListener(OnClickNext);
+
+        nextButton.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void minigameStart()
     {
-        
+        activePart.SetActive(true);
+    }
+
+    private void OnClickGood()
+    {
+        quizGood.SetActive(true);
+        quizBad1.SetActive(false);
+        quizBad2.SetActive(false);
+        nextButton.SetActive(true);
+    }
+
+    private void OnClickBad()
+    {
+        nextButton.SetActive(false);
+    }
+
+    private void OnClickNext()
+    {
+        activePart.SetActive(false);
+        nextPart.SetActive(true);
     }
 }
