a5219bc451af0c632154750237fb9d82909ad230
Thomas <t.rompre@student.isartdigital.com>
2020-05-20 11:37:40 -0400

    [Partition] lunchtime push

--- Assets/MiniGames/Partition/script/NoteScript.cs
+++ Assets/MiniGames/Partition/script/NoteScript.cs
@@ -6,17 +6,34 @@ public class NoteScript : MonoBehaviour
 {
 
     private AudioSource AUDIO;
+    private Camera CAMERA;
+    private GameObject NOTEHOLDER;
 
     // Start is called before the first frame update
     void Start()
     {
         AUDIO = gameObject.transform.GetComponent<AudioSource>();
+        CAMERA = GameObject.Find("Main Camera").transform.GetComponent<Camera>();
+        NOTEHOLDER = transform.parent.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = CAMERA.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                Debug.Log("looks live youve clicked on " + hit.transform.name);
+            }
+        }
+
+        if (Input.GetKeyDown("space")) {
+            // Debug.Log(transform.parent.transform.childCount);
+            //Debug.Log(transform.GetSiblingIndex());
+        }
         
+
     }
 
     void OnTriggerEnter(Collider other) {
