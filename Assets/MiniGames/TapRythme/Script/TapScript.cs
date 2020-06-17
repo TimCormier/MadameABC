72ef3a6e3d2f7cc733ac6e85de7a3b49b50b5635
Tim <t.cormier@student.isartdigital.com>
2020-06-17 13:33:27 -0400

    [TYapRythme] 1st push

---
+++ Assets/MiniGames/TapRythme/Script/TapScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapScript : MonoBehaviour
+{
+    public GameObject noteOnCollider;
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && noteOnCollider != null)
+            noteOnCollider.SetActive(false);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        noteOnCollider = collision.gameObject;
+    }
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        noteOnCollider = null;
+    }
+}
