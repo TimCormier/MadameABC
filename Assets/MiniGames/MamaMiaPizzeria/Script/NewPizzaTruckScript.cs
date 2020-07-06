5cb41c54f654b9638ed16fbfeec8b40a3022cc73
Thomas <t.rompre@student.isartdigital.com>
2020-07-06 17:13:28 -0400

    [MamaMiaPIzzeria] Houses added, scoring system and delivery system is added.

--- Assets/MiniGames/MamaMiaPizzeria/Script/NewPizzaTruckScript.cs
+++ Assets/MiniGames/MamaMiaPizzeria/Script/NewPizzaTruckScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewPizzaTruckScript : MonoBehaviour
 {
@@ -18,9 +19,16 @@ public class NewPizzaTruckScript : MonoBehaviour
 
 
     private float velocity;
+
+    private string order;
+    private int score;
+
+    private Text scoretext;
     // Start is called before the first frame update
     void Start()
     {
+        score = 0;
+        scoretext = GameObject.Find("Text").GetComponent<Text>();
         PizzaSpawner = GameObject.Find("PizzaSpawner");
         Debug.Log(PizzaSpawner.transform.name);
         stopTimer = 0f;
@@ -37,6 +45,10 @@ public class NewPizzaTruckScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown("space")) {
+            order = "Blue";
+        }
+
 
         //Input
         if (Input.GetKey("up")) {
@@ -88,19 +100,123 @@ public class NewPizzaTruckScript : MonoBehaviour
     }
 
     void OnTriggerEnter(Collider col) {
-        if (col.transform.name == "PizzeriaTrigger") {
-            Debug.Log("Youre at the pizza place");
-            if (HasPizza == false) {
-                Instantiate(PizzaBox, PizzaSpawner.transform.position, PizzaSpawner.transform.rotation);
-                GameObject.FindWithTag("Pizza").transform.SetParent(gameObject.transform);
-                HasPizza = true;
-              /*  GameObject[] Pizzalist = GameObject.FindGameObjectsWithTag("Pizza");
-                foreach (GameObject pizza in Pizzalist)
+        switch (col.transform.name) {
+            case "PizzeriaTrigger":
+               // Debug.Log("Youre at the pizza place");
+                if (HasPizza == false)
                 {
-                    Physics.IgnoreCollision(pizza.transform.GetComponent<Collider>(), gameObject.transform.GetComponent<CharacterController>());
-                }*/
-            }
+                    ORDER();
+                    /*  GameObject[] Pizzalist = GameObject.FindGameObjectsWithTag("Pizza");
+                      foreach (GameObject pizza in Pizzalist)
+                      {
+                          Physics.IgnoreCollision(pizza.transform.GetComponent<Collider>(), gameObject.transform.GetComponent<CharacterController>());
+                      }*/
+                }
+                break;
+
+            case "BlueHouseTrigger":
+               // Debug.Log("You're at the blue house!");
+                if (HasPizza == true && order == "Blue") {
+                    Destroy(GameObject.FindWithTag("Pizza"));
+                    HasPizza = false;
+                    score++;
+                }
+
+                break;
+
+            case "RedHouseTrigger":
+               // Debug.Log("You're at the Red house!");
+                if (HasPizza == true && order == "Red")
+                {
+                    Destroy(GameObject.FindWithTag("Pizza"));
+                    HasPizza = false;
+                    score++;
+                }
+
+                break;
+
+            case "YellowHouseTrigger":
+               // Debug.Log("You're at the Yellow house!");
+                if (HasPizza == true && order == "Yellow")
+                {
+                    Destroy(GameObject.FindWithTag("Pizza"));
+                    HasPizza = false;
+                    score++;
+                }
+
+                break;
+
+            case "PurpleHouseTrigger":
+               // Debug.Log("You're at the blue house!");
+                if (HasPizza == true && order == "Purple")
+                {
+                    Destroy(GameObject.FindWithTag("Pizza"));
+                    HasPizza = false;
+                    score++;
+                }
+
+                break;
+
+            case "GreenHouseTrigger":
+               // Debug.Log("You're at the blue house!");
+                if (HasPizza == true && order == "Green")
+                {
+                    Destroy(GameObject.FindWithTag("Pizza"));
+                    HasPizza = false;
+                    score++;
+                }
+
+                break;
+
+            default:
+                Debug.Log("Wrong collider");
+                break;
+
         }
+
+        scoretext.text = score.ToString(); ;
+
+
+
+        /*if (col.transform.name == "PizzeriaTrigger") {
+            
+        }*/
+    }
+
+
+    public void ORDER() {
+        Instantiate(PizzaBox, PizzaSpawner.transform.position, PizzaSpawner.transform.rotation);
+        GameObject.FindWithTag("Pizza").transform.SetParent(gameObject.transform);
+        HasPizza = true;
+        float id = Mathf.Round(Random.Range(1f, 5f));
+
+        switch (id) {
+            case 1f:
+                order = "Blue";
+                break;
+
+            case 2f:
+                order = "Red";
+                break;
+
+            case 3f:
+                order = "Yellow";
+                break;
+
+            case 4f:
+                order = "Purple";
+                break;
+
+            case 5f:
+                order = "Green";
+                break;
+
+            default:
+                Debug.Log("id out of bounds");
+                break;
+        }
+
+        Debug.Log("Order is " + order);
     }
 
 }
