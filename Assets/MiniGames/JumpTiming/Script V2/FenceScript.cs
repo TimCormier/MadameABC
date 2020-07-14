10a9e736ce4a8f10bc6374d9393904963a5f247d
Thomas <t.rompre@student.isartdigital.com>
2020-07-14 16:50:50 -0400

    [JumpTimingV2] should be done around noon tomorrow or earlier, game is functional just needs increasing difficulty to be coded in

---
+++ Assets/MiniGames/JumpTiming/Script V2/FenceScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FenceScript : MonoBehaviour
+{
+
+    public float speed;
+    private GameObject PLAYER;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PLAYER = GameObject.Find("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position -= new Vector3(speed, 0f, 0f);
+
+        if (gameObject.transform.position.x <= -150f) {
+            PLAYER.GetComponent<SheepScript>().SPAWNFENCE();
+            Destroy(gameObject);
+        }
+      //  Debug.Log("Fence " + gameObject.transform.name + " is at x " + gameObject.transform.position.x);
+    }
+}
---
+++ Assets/MiniGames/JumpTiming/Script V2/SheepScript.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SheepScript : MonoBehaviour
+{
+    private GameObject PLAYER;
+    private float velocity;
+    public float gravity;
+    public float JumpStrength;
+
+    private bool jumping = false;
+    private float jumpTimer = 0f;
+    public float jumpTimerLimit;
+
+    private bool grounded = false;
+
+    private GameObject FenceSpawner;
+    public GameObject Fence;
+    private GameObject canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = GameObject.Find("Canvas");
+        FenceSpawner = GameObject.Find("FenceSpawner");
+        PLAYER = GameObject.Find("Player");
+        velocity = 0f;
+
+        SPAWNFENCE();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            //Debug.Log("Click");
+            if (grounded == true) {
+                jumping = true;
+                grounded = false;
+                velocity += JumpStrength;
+                
+            }
+           
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 8f)) {
+            if (hit.transform.name == "Ground") {
+               // Debug.Log("Raycast hit ground");
+                if (jumping == false) {
+                    velocity = 0;
+                    grounded = true;
+                }
+                
+            }
+           
+        }
+
+        if (jumping == true) {
+            jumpTimer += Time.deltaTime;
+            if (jumpTimer >= jumpTimerLimit) {
+                jumping = false;
+            }
+        }
+
+
+        if (grounded == false) {
+            velocity -= gravity;
+            PLAYER.transform.position += new Vector3(0f, velocity, 0f);
+        }
+       // Debug.Log("velocity is " + velocity);
+    }
+
+    public void SPAWNFENCE() {
+        Instantiate(Fence, FenceSpawner.transform.position, FenceSpawner.transform.rotation, canvas.transform);
+    }
+
+    void OnTriggerEnter(Collider col) {
+        if (col.transform.tag == "FENCE") {
+            Debug.Log("touched fence");
+            string temp = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(temp);
+        }
+    }
+}
