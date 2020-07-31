31eeb139247981dbbdce10dc9473e7e46986290a
Anthony Teixeira <anthony.teixeira.at@gmail.com>
2020-07-31 10:57:26 -0400

    [Particles Feedback] Camera switch for particle system

---
+++ Assets/Feedbacks/ParticleUpLaunch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleUpLaunch : MonoBehaviour
+{
+    //To use, change your canvas's render mode to Screen Space - Camera
+
+    public ParticleSystem particleLauncher;
+    public float particleRelauncher;
+
+    void Start()
+    {
+        particleRelauncher = 0f;
+    }
+
+
+    void Update()
+    {
+        if(particleRelauncher >= 1)
+        {
+            particleLauncher.Emit(5);
+            particleRelauncher -= 1;
+        }
+    }
+
+    public void LaunchParticle()
+    {
+        particleRelauncher += 1;
+    }
+}
